8c560934f603f1df07ab66c49c3f2bef51ed324c
hunit <hunit612@gmail.com>
2021-05-11 00:15:20 +0900

    create login test page

--- instagram/apis/views.py
+++ instagram/apis/views.py
@@ -4,6 +4,8 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views import View
 from django.contrib.auth.models import User
 from django.db import IntegrityError
+from django.core.validators import validate_email, ValidationError
+from django.contrib.auth import authenticate, login
 
 
 class BaseView(View):
@@ -29,8 +31,10 @@ class UserCreateView(BaseView):
         if not password:
             return self.response(message='패스워드를 입력해주세요.', status=400)
         email = request.POST.get('email', '')
-        if not email:
-            return self.response(message='올바른 이메일을 입력해주세요.', status=400)
+        try:
+            validate_email(email)
+        except ValidationError:
+            self.response(message='올바른 이메일을 입력해주세요.', status=400)
 
         try:
             user = User.object.create_user(username, password, email)
@@ -38,3 +42,20 @@ class UserCreateView(BaseView):
             return self.response(message='이미 존재하는 아이디입니다.', status=400)
 
         return self.response({'user.id':user.id})
+
+
+class UserLoginView(BaseView):
+    def post(self, request):
+        username = request.POST.get('username', '')
+        if not username:
+            return self.response(message='아이디를 입력해주세요', status=400)
+        password = request.POST.get('password', '')
+        if not password:
+            return self.response(message='패스워드를 입력해주세요.', status=400)
+
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            return self.response(message='입력정보를 확인해주세요.', status=400)
+        login(request, user)
+
+        return self.response
